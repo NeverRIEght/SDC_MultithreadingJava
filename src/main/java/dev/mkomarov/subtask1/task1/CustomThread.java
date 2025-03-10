c18306fe56905a0bb9a47f3fd7cbb4ad83aaaef2
Mihail Komarov <michaelkomarov03@gmail.com>
2025-03-10 16:16:50 +0200

    fix code conventions for subtask1.task1

--- src/main/java/dev/mkomarov/subtask1/task1/CustomThread.java
+++ src/main/java/dev/mkomarov/subtask1/task1/CustomThread.java
@@ -3,8 +3,8 @@ package dev.mkomarov.subtask1.task1;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class CustomThread extends Thread {
-    private static final Logger log = LoggerFactory.getLogger(CustomThread.class);
+class CustomThread extends Thread {
+    private static final Logger LOG = LoggerFactory.getLogger(CustomThread.class);
     private final int numberOfStrings;
 
     CustomThread(int numberOfStrings) {
@@ -14,7 +14,7 @@ public class CustomThread extends Thread {
     @Override
     public void run() {
         for (int i = 0; i < numberOfStrings; i++) {
-            log.info("{} {}", Thread.currentThread().getName(), i);
+            LOG.info("{} {}", Thread.currentThread().getName(), i);
         }
     }
 }
--- src/main/java/dev/mkomarov/subtask1/task1/Main.java
+++ src/main/java/dev/mkomarov/subtask1/task1/Main.java
@@ -3,16 +3,15 @@ package dev.mkomarov.subtask1.task1;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Main {
-    private static final Logger log = LoggerFactory.getLogger(Main.class);
-    static int NUMBER_OF_STRINGS = 1000;
+class Main {
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+    private static final int NUMBER_OF_STRINGS = 1000;
 
     public static void main(String[] args) {
-        CustomThread t0 = new CustomThread(NUMBER_OF_STRINGS);
-        t0.start();
+        new CustomThread(NUMBER_OF_STRINGS).start();
 
         for (int i = 0; i < NUMBER_OF_STRINGS; i++) {
-            log.info("{} {}", Thread.currentThread().getName(), i);
+            LOG.info("{} {}", Thread.currentThread().getName(), i);
         }
     }
 }
