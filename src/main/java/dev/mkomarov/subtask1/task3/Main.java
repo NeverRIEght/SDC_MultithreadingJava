135f9b6668e3081dc6f100b39403be474cde227a
Mihail Komarov <michaelkomarov03@gmail.com>
2025-03-16 19:58:55 +0200

    fix code conventions for subtask1.task3

--- src/main/java/dev/mkomarov/subtask1/task3/Main.java
+++ src/main/java/dev/mkomarov/subtask1/task3/Main.java
@@ -3,9 +3,9 @@ package dev.mkomarov.subtask1.task3;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class Main {
-    private static final Logger log = LoggerFactory.getLogger(Main.class);
-    static int NUMBER_OF_STRINGS = 1000;
+class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    private static final int NUMBER_OF_STRINGS = 1000;
 
     public static void main(String[] args) throws InterruptedException {
         CustomThread t0 = new CustomThread(NUMBER_OF_STRINGS);
@@ -15,7 +15,7 @@ public class Main {
         t0.join();
 
         for (int i = 0; i < NUMBER_OF_STRINGS; i++) {
-            log.info("{} {}", Thread.currentThread().getName(), i);
+            logger.info("{} {}", Thread.currentThread().getName(), i);
         }
     }
 }
